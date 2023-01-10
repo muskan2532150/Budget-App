b39a74612dce74dba0515f4f93eb77e55bf85c40
muskan2532150 <rg2532150@gmail.com>
2023-01-10 20:32:26 +0530

    Add routes and signparams action

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+resources :group do
+  resources :entity
+end
+
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "group#index"
 end
