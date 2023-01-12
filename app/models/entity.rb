7fc348ae22e51ce15768e438e9805e398353a1ca
muskan2532150 <rg2532150@gmail.com>
2023-01-12 22:06:01 +0530

    Authorization

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,2 +1,5 @@
 class Entity < ApplicationRecord
+    belong_to :user
+    validates :name, presence:true
+    validates :amount, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,6 @@
 class Group < ApplicationRecord
+    belongs_to :user
+    has_many :entitys, dependent: :destroy
+
+    validates :name, presence:true
 end
