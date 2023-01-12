6908fd8d59e4f8a9dcacede12a2eb04d67a1b7a4
muskan2532150 <rg2532150@gmail.com>
2023-01-13 00:10:30 +0530

    Add splash and update style

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  root "splashs#index"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 resources :groups do
   resources :entitys
 end
-  # Defines the root path route ("/")
-  root "groups#index"
+  # Defines the root path route ("/")  
 end
