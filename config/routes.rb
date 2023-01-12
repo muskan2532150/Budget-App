9f300e147fec44450ef81683f94b92348f852fcc
muskan2532150 <rg2532150@gmail.com>
2023-01-12 22:08:07 +0530

    Fix route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-resources :group do
-  resources :entity
+resources :groups do
+  resources :entitys
 end
-
   # Defines the root path route ("/")
-  root "group#index"
+  root "groups#index"
 end
