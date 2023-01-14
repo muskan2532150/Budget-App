aaea707eed113ad43212e1056f8674174b59763f
muskan2532150 <rg2532150@gmail.com>
2023-01-14 21:58:57 +0530

    Fix rubocop error

--- spec/request/entity_spec.rb
+++ spec/request/entity_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Entity', type: :request do
   before(:each) do
-    user = User.create(name:'Muskan', email: 'tom@example.com', password: '123456')
+    user = User.create(name: 'Muskan', email: 'tom@example.com', password: '123456')
     post user_session_path, params: { user: { name: 'Muskan', email: user.email, password: user.password } }
     get root_path
 
