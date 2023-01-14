896bfad34fbb24f4d76d19136fd5b4ce6b54afce
muskan2532150 <rg2532150@gmail.com>
2023-01-14 18:00:10 +0530

    Fix devise error,update css

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-root "splashs#index"
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 resources :groups do
   resources :entitys
 end
   # Defines the root path route ("/")  
+  root "splashs#index"
 end
